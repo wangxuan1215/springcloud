32e9e6e6dc054d7af2747525937585f4bb1c8618
wangxuan <228913120@qq.com>
2020-09-21 11:00:01 +0800

    gateway集群

---
+++ gateway002/src/test/java/com/wangx/gateway002/Gateway002ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangx.gateway002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gateway002ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
