43a89f5cc491b85e0119d7c8813246439323457d
wangxuan <228913120@qq.com>
2020-10-08 21:59:24 +0800

    新增认证中心

---
+++ authcenter/src/main/java/com/wangx/authcenter/AuthcenterApplication.java
@@ -0,0 +1,13 @@
+package com.wangx.authcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthcenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthcenterApplication.class, args);
+    }
+
+}
---
+++ authcenter/src/test/java/com/wangx/authcenter/AuthcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangx.authcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthcenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
